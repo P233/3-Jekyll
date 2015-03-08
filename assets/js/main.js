3d03ff9d91afbf239f13d5b0a2018e7087299051
Peiwen Lu <hi@peiwen.lu>
2015-03-08 23:52:33 +0000

    2015 new version

---
+++ assets/js/main.js
@@ -0,0 +1,75 @@
+---
+layout: null
+---
+
+$(function() {
+  var toc     = $('.toc-link'),
+      sidebar = $('#sidebar'),
+      main    = $('#main'),
+      menu    = $('#menu'),
+      x1, y1;
+
+  // run this function after pjax load.
+  var afterPjax = function() {
+    // open links in new tab.
+    $('#main').find('a').filter(function() {
+      return this.hostname != window.location.hostname;
+    }).attr('target', '_blank');
+
+    // discus comment.
+    {% if site.disqus_shortname %}
+    (function() {
+      var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+      dsq.src = '//{{ site.disqus_shortname }}' + '.disqus.com/embed.js';
+      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
+    {% endif %}
+
+    // your scripts
+  };
+  afterPjax();
+
+
+  // NProgress
+  NProgress.configure({ showSpinner: false });
+
+
+  // Pjax
+  $(document).pjax('#sidebar-avatar, .toc-link', '#main', {
+    fragment: '#main',
+    timeout: 3000
+  });
+
+  $(document).on({
+    'pjax:click': function() {
+      NProgress.start();
+      main.removeClass('fadeIn');
+    },
+    'pjax:end': function() {
+      afterPjax();
+      NProgress.done();
+      main.scrollTop(0).addClass('fadeIn');
+      menu.add(sidebar).removeClass('open');
+    }
+  });
+
+
+  // Tags Filter
+  $('#sidebar-tags').on('click', '.sidebar-tag', function() {
+    var filter = $(this).data('filter');
+    if (filter === 'all') {
+      toc.fadeIn(350);
+    } else {
+      toc.hide();
+      $('.toc-link[data-tags~=' + filter + ']').fadeIn(350);
+    }
+    $(this).addClass('active').siblings().removeClass('active');
+  });
+
+
+  // Menu
+  menu.on('click', function() {
+    $(this).add(sidebar).toggleClass('open');
+  });
+
+});
